b099d25e51f6cb3ca1ec69fe6774516ef234555e
Dale Syth <dalesyth@gmail.com>
2023-07-06 22:12:03 -0500

    add JSON web tokens

--- db/index.js
+++ db/index.js
@@ -343,6 +343,20 @@ async function getAllTags() {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `, [username]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
   createUser,
@@ -357,4 +371,5 @@ module.exports = {
   createTags,
   getPostsByTagName,
   getAllTags,
+  getUserByUsername,
 };
