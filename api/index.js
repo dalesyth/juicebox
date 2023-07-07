68019225d9f4ce5a0692d0ba66cca85f38bc7dc2
Dale Syth <dalesyth@gmail.com>
2023-07-06 19:59:48 -0500

    added posts router

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
-const express = require('express');
+const express = require("express");
 const apiRouter = express.Router();
 
-const usersRouter = require('./users');
-apiRouter.use('/users', usersRouter);
+const usersRouter = require("./users");
+apiRouter.use("/users", usersRouter);
+
+const postsRouter = require("./posts");
+apiRouter.use("/posts", postsRouter);
 
 module.exports = apiRouter;
