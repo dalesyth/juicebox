d62cf0124527951f88989df5afb356a7c93fb44e
Dale Syth <dalesyth@gmail.com>
2023-07-08 15:12:18 -0500

    writing POST /api/posts

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,34 @@
 const express = require("express");
 const postsRouter = express.Router();
 
-const { getAllPosts } = require("../db");
+const { getAllPosts, createPost } = require("../db");
+
+const { requireUser } = require("./utils");
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData.authorId = users.id;
+    postData.title = title;
+    postData.content = content;
+    // const post = await createPost(postData);
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
